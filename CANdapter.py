c907746666c03d22a89eafc5424fd6b50d36a03e
tazukiswift <toby.osbornes.nz@gmail.com>
2023-11-04 18:37:15 +1300

    implemented chris ros2 python

--- CANdapter.py
+++ CANdapter.py
@@ -1,5 +1,6 @@
 import serial
-
+import re
+import threading
 
 CR = '\015'
 
@@ -31,31 +32,124 @@ class CANFrame:
         return bytes(identifier, 'ascii') + bytes(length, 'ascii') + data
 
 
+def parse_can_frame(frame):
+    the_id = int(frame[:3], 16)
+    length = int(frame[3:4], 16)
+    data = [int(frame[4 + i:4 + i + 2], 16) if i + 4 < len(frame) else 0 for i in range(8)]
+
+    return the_id, length, data
+
+
+def get_baud_rate_command(rate: int):
+    command = b'S5'
+
+    if rate == 10:
+        command = b'S0'
+    elif rate == 20:
+        command = b'S1'
+    elif rate == 50:
+        command = b'S2'
+    elif rate == 100:
+        command = b'S3'
+    elif rate == 125:
+        command = b'S4'
+    elif rate == 250:
+        command = b'S5'
+    elif rate == 500:
+        command = b'S6'
+    elif rate == 800:
+        command = b'S7'
+    elif rate == 1000:
+        command = b'S8'
+
+    return command
+
+
+def clean_can_frame(data):
+    frames = re.sub(r'[^a-zA-Z0-9]', '', data)
+    frames = frames.split('t')
+    frames.remove('')
+    return frames
+
+
 class CANDapter:
-    def __init__(self, port, debug=True):
-        self.debug = debug
-        self.port = port
-        self.serial = serial.Serial(port)
+    baud_rate = 250
+    serial_port = 0
+
+    def __init__(self, port, baud_rate):
+        # self.subs = subs
+        self.serial_port = serial.Serial(
+            port
+        )
+
+        self.send_command_with_response(b'C')  # restart can dapter
+
+        while not self.send_command_with_response(
+                get_baud_rate_command(self.baud_rate)
+        ):
+            pass
+
+        while not self.send_command_with_response(b'O'):  # start can dapter
+            pass
+
+        self.serial_thread = threading.Thread(target=self.read_serial)
+        # self.serial_thread.daemon = True
+        self.serial_thread.start()
+
+    def send_command_with_response(self, command: bytes):
+        # NOTE DO NOT RUN THIS IF YOU HAVE READING THREAD OPEN
+        if not self.serial_port.is_open:
+            return False
+
+        msg = b'\015' + command + b'\015'
+        self.serial_port.write(msg)
+        response = self.serial_port.read()
+
+        print(f'Sent cmd: {command} | Response: {response}')
+        return response == b'\06'
+
+    def send_command(self, command: bytes):
+        if not self.serial_port.is_open:
+            return False
+
+        msg = b'\015' + command + b'\015'
+        self.serial_port.write(msg)
+        return True
+
+    def can_send_callback(self, can_frame):
+        self.send_command(
+            f'T{can_frame.std_id:03X}'
+            f'{len(can_frame.data):01X}'
+            f'{"".join(["%02X" % byte for byte in can_frame.data])}'
+            .encode())
+
+    def read_serial(self):
+        while True:
+            if self.serial_port.in_waiting > 0:
+                data = self.serial_port.read_until(b'\r') \
+                    .decode() \
+                    .strip()
+
+                try:
+                    index_of_t = data.index('t')
+                    data = data[index_of_t:]
+                except ValueError:
+                    pass
+
+                # if data[0] == 't':
+                #     cleaned_data_list = clean_can_frame(data)
+                #     for cleaned_data in cleaned_data_list:
+                #         try:
+                #             frame = parse_can_frame(cleaned_data)
+                #             for sub in self.subs:
+                #                 sub.event.set()
+                #                 sub.message = frame
+                #         except:
+                #             print('')
 
     def send_can_message(self, can_frame: CANFrame):
         command = b'T' + can_frame.to_bytes()
         self.send_command(command)
-
-    def send_command(self, command: bytes):
-        if not self.serial.is_open:
-            return False
-
-        msg = b'\015' + command + b'\015'
-        self.serial.write(msg)
-        return_message = self.serial.read()
-
-        if self.debug:
-            print(f'------- CANDapter Command sent --------')
-            print(f'Command sent: {msg}')
-            print(f'Received Value: {return_message}')
-            print(f'---------------------------------------')
-
-        return return_message == b'\x06'
 
     def set_bitrate(self, rate: int) -> bool:
         """https://www.ewertenergy.com/products/candapter/downloads/candapter_manual.pdf
@@ -102,7 +196,7 @@ class CANDapter:
         return self.send_command(b'C')
 
     def _read_until(self):
-        return self.serial.read_until(expected=b'\r')
+        return self.serial_port.read_until(expected=b'\r')
 
     def read_can_message(self):
         can_message = str(self._read_until())[3:-3]
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from CANdapter import CANDapter, CANFrame
 import threading
 import time
 
+def crc(message):
+    message.data[7] = sum(message.data[:-1]) % 256
 
 def call_every(interval, func, *args):
     while True:
@@ -11,32 +13,40 @@ def call_every(interval, func, *args):
         time.sleep(max(0, interval - elapsed))
 
 
+def call_constantly(func, *args):
+    while True:
+        func(*args)
+
+
 def send_motec_keepalive(candapter, frame0x100):
     candapter.send_can_message(frame0x100)
 
 
 def send_packstate_2(candapter: CANDapter, bms_active, frame0x6B1: CANFrame):
     frame0x6B1.data[6] = not frame0x6B1.data[7]  # change rolling counter
-    # TODO: ORION CHECKSUM, 8th bit should always be the checksum
-    # frame0x6B1.data[7]
+    crc(frame0x6B1)
     if bms_active:
         candapter.send_can_message(frame0x6B1)
 
 
 def send_cell_votages_temperatures(candapter, bms_active, frame0x6B3, frame0x6B4):
-    # TODO: ORION CHECKSUM, 8th bit should always be the checksum
-    # frame0x6B3.data[7]
-    # frame0x6B4.data[7]
+    crc(frame0x6B3)
+    crc(frame0x6B4)
+
     if bms_active:
         candapter.send_can_message(frame0x6B3)
         candapter.send_can_message(frame0x6B4)
 
 
+def handle_received_message(candapter):
+    candapter.read_can_message()
+
+
 def main():
-    candapter = CANDapter(port="COM3")
+    candapter = CANDapter("/dev/ttyUSB0", 250)
 
     # defines
-    bms_active = 0
+    bms_active = [0]
     motec_keepalive = 0
     high_volt = 3.8
     low_volt = 3.5
@@ -45,16 +55,11 @@ def main():
 
     # Frames can be explained at https://docs.fsae.co.nz/en/Electrons/HV/Accumulator/BMSEmulator
     # some have been excluded since i don't think they matter (6B0, 6B2, 618)
-    frame0x100 = CANFrame(100, 1, [0])
-    frame0x6B1 = CANFrame(100, 8, [0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 1, 0])  # no vars as bit 7 set in thread
+    frame0x100 = CANFrame("100", 1, [0x0])
+    frame0x6B1 = CANFrame("6B1", 8, [0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x1, 0x0])  # no vars as bit 7 set in thread
     # TODO: check if CAN is big or little endian
-    frame0x6B3 = CANFrame(100, 8, [0x00, high_volt * 1000, 0x00, 0xFF, 0x00, low_volt * 1000, num_cells])
-    frame0x6B4 = CANFrame(100, 8, [0x00, high_temp, 0x00, 0x13, 0x00, 0x05, 0, 0])
-
-    # Open Candapter channel
-    candapter.close_channel()
-    candapter.set_bitrate(250)
-    candapter.open_channel()
+    frame0x6B3 = CANFrame("6B3", 8, [0x00, int(high_volt * 1000), 0x00, 0xFF, 0x00, int(low_volt * 1000), int(num_cells), 0x0])
+    frame0x6B4 = CANFrame("6B4", 8, [0x00, int(high_temp), 0x00, 0x13, 0x00, 0x05, 0x0, 0x0])
 
     threading.Thread(target=call_every, args=(0.048, send_motec_keepalive, candapter, frame0x100)).start()
     threading.Thread(target=call_every, args=(0.152, send_packstate_2, candapter, bms_active, frame0x6B1)).start()
@@ -65,7 +70,8 @@ def main():
         user_input = input("Enter a character (m, b, v, t): ")
         if user_input == 'm':  # motec keepalive
             print("Keepalive toggles")
-            frame0x100.data = not frame0x100.data
+            frame0x100.data[0] = not frame0x100.data[0]
+            motec_keepalive = not motec_keepalive
         elif user_input == 'b':  # BMS active
             print("BMSActive toggles")
             bms_active = not bms_active
